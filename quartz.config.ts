2722ee557bec4dddd1107208dd828ffe5f3e7db9
shivangjhalani <shivang2004jhalani@gmail.com>
2025-03-16 12:29:40 +0530

    Quartz sync: Mar 16, 2025, 12:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Shivang Jhalani",
+    pageTitle: "Shivang",
     pageTitleSuffix: " | Shivang Jhalani",
     enableSPA: true,
     enablePopovers: true,
